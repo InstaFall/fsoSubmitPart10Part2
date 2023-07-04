1dfb14f80579ae292c1d5b2592bad507912f0425
InstaFall <gorkemcanakilli@hotmail.com>
2023-07-04 23:07:59 +0300

    Commit exercise 10.6

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
 import { View, StyleSheet, Pressable } from 'react-native';
 import Constants from 'expo-constants';
+import { Link } from 'react-router-native';
 import Text from './Text';
 import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    paddingBottom: 10,
-    paddingHorizontal: 15,
+    
     backgroundColor: theme.colors.appBar,    
+    flexDirection: "row", 
+  },
+  tab: {
+    flexGrow: 1,
+    flexBasis: 0,
+    alignItems: "center",
   },
   tabText: {
-    color: "#fff",
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: theme.fontSizes.subheading,
+    padding: 10,
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable style={styles.tab} onPress={() => {}}>
-        <Text style={styles.tabText} fontWeight="bold" >Repositories</Text>
-      </Pressable>
+      <Link to="/" component={Pressable} style={styles.tab} onPress={() => {}}>
+        <Text style={styles.tabText}>Repositories</Text>
+      </Link>
+      <Link to="/signin" component={Pressable} style={styles.tab} onPress={() => {}}>
+        <Text style={styles.tabText}>Sign in</Text>
+      </Link>
     </View>
   );
 };
