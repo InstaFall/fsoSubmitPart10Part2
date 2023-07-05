2ea6c0362f0ae6d4ad12a9d968b8cce4ab22f956
InstaFall <gorkemcanakilli@hotmail.com>
2023-07-06 00:05:16 +0300

    Commit exercise 10.8

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,72 @@
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
+import theme from '../theme';
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View style={styles.topView}>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={onSubmit}
+      >
+        {({ handleSubmit }) => (
+          <View style={styles.container}>
+            <FormikTextInput
+              name="username"
+              placeholder="Username"
+              style={styles.input}
+            />
+            <FormikTextInput
+              name="password"
+              placeholder="Password"
+              secureTextEntry
+              style={styles.input}
+            />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.buttonText} fontWeight="bold" >SUBMIT</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  topView: {
+    backgroundColor: "#fff",
+    paddingBottom: 10
+  },
+  container: {
+    marginHorizontal: 10,
+  },
+  input: {
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderRadius: 5,
+  },
+  button: {
+    marginTop: 10,
+    paddingVertical: 10,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+  },
+});
 
 export default SignIn;
